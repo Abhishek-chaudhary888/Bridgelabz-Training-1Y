33a2f0e33e70f71c4247fb9e1c543fbaa16fd92d
Abhishek-chaudhary888 <abhishekchaudhary7745@gmail.com>
2026-04-10 11:50:05 +0600

    Logistics

---
+++ Logistics/src/com/gla/Checkpoint.java
@@ -0,0 +1,35 @@
+package com.gla;
+
+public class Checkpoint {
+
+    String checkpointId;
+    String locationName;
+    double distanceFromlast;
+    int expectedDuration;
+    int actualDuration;
+
+}
+abstract class Checkpoint {
+    String checkpointId;
+    String locationName;
+    double distanceFromLast;
+    int expectedDuration;
+    int actualDuration;
+
+    public Checkpoint(String checkpointId, String locationName,
+                      double distanceFromLast, int expectedDuration, int actualDuration) {
+        this.checkpointId = checkpointId;
+        this.locationName = locationName;
+        this.distanceFromLast = distanceFromLast;
+        this.expectedDuration = expectedDuration;
+        this.actualDuration = actualDuration;
+    }
+
+    boolean isDelayed() {
+        return actualDuration > expectedDuration;
+    }
+
+    abstract boolean isCritical();
+    abstract String getType();
+    abstract double calculatePenalty();
+}
---
+++ Logistics/src/com/gla/FuelCheckpoint.java
@@ -0,0 +1,16 @@
+package com.gla;
+
+class FuelCheckpoint extends Checkpoint {
+
+    public FuelCheckpoint(String id, String loc, double dist, int exp, int act) {
+        super(id, loc, dist, exp, act);
+    }
+
+    boolean isCritical() { return true; }
+
+    String getType() { return "FuelCheckpoint"; }
+
+    double calculatePenalty() {
+        return isDelayed() ? 10 : 0;
+    }
+}
